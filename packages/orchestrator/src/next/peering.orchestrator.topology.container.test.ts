5ec2a3699f0f1210af087055edf57c0946655542
J Taylor <j.taylor@orbisops.com>
2026-01-27 06:17:21 -0800

    chore: removing flexibility to only use podman

--- packages/orchestrator/src/next/peering.orchestrator.topology.container.test.ts
+++ packages/orchestrator/src/next/peering.orchestrator.topology.container.test.ts
@@ -20,8 +20,7 @@ describe('Orchestrator Peering Container Tests', () => {
 
     const orchestratorImage = 'localhost/catalyst-node:next-topology-e2e'
     const repoRoot = path.resolve(__dirname, '../../../../')
-    const skipTests =
-        !process.env.DOCKER_HOST && !process.env.DOCKER_SOCK && !process.env.PODMAN_VAR_LINK
+    const skipTests = !process.env.CATALYST_CONTAINER_TESTS_ENABLED
 
     beforeAll(async () => {
         if (skipTests) {
